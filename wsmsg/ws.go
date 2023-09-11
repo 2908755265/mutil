5ac803f0efb3d7d0ee60056c47c544ea34099380
MackWang <18583618603@163.com>
2023-09-11 16:26:11 +0800

    err handler

--- wsmsg/ws.go
+++ wsmsg/ws.go
@@ -15,8 +15,11 @@ type MessagePusher interface {
 	Push(key string, val any) error
 }
 
+type ErrHandler func(key string, val any, conn *websocket.Conn, wrapper *WsPusher)
+
 type WsPusher struct {
 	msync.MapInterface[string, *websocket.Conn]
+	errHandler ErrHandler
 }
 
 func (p *WsPusher) Push(key string, val any) error {
@@ -30,11 +33,25 @@ func (p *WsPusher) Push(key string, val any) error {
 		return err
 	}
 
-	return conn.WriteMessage(websocket.TextMessage, bts)
+	err = conn.WriteMessage(websocket.TextMessage, bts)
+	if err != nil {
+		p.errHandler(key, val, conn, p)
+	}
+	return err
+}
+
+func defaultErrHandler(key string, _ any, conn *websocket.Conn, wrapper *WsPusher) {
+	conn.Close()
+	wrapper.Delete(key)
 }
 
 func NewWSPusher() *WsPusher {
 	return &WsPusher{
 		MapInterface: msync.NewSyncMap[string, *websocket.Conn](),
+		errHandler:   defaultErrHandler,
 	}
+}
+
+func WithErrorHandler(p *WsPusher, h ErrHandler) {
+	p.errHandler = h
 }
